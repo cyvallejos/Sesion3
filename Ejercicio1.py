daf466ee8c90036e533bce109ef0e5c626922528
camila <cyvallejos@uamv.edu.ni>
2026-08-21 10:30:45 -0600

    first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+carrera = input("Ingrese su carrera: ")
+
+print(f"Mi nombre es {nombre}, tengo {edad} años y estudio {carrera}.")
